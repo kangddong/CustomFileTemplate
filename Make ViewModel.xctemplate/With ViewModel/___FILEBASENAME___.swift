77fa19938c224e246b5ddd2d8a22be07aed66e1b
kangddong <rkdehddud96@naver.com>
2022-11-30 23:12:23 +0900

    init

---
+++ Make ViewModel.xctemplate/With ViewModel/___FILEBASENAME___.swift
@@ -0,0 +1,24 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___VARIABLE_productName:identifier___ViewController: UIViewController {
+
+    
+    var viewModel: ___VARIABLE_productName:identifier___ViewModelAvailable?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initLayout()
+        bindViewModel()
+    }
+    
+    private func initLayout() {
+        
+    }
+    
+    private func bindViewModel() {
+        
+    }
+}
---
+++ Make ViewModel.xctemplate/With ViewModel/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,11 @@
+//___FILEHEADER___
+
+import Foundation
+
+protocol ___VARIABLE_productName:identifier___ViewModelAvailable {
+    
+}
+
+class ___VARIABLE_productName:identifier___ViewModel: ___VARIABLE_productName:identifier___ViewModelAvailable {
+
+}
